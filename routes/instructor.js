6da9cbabf869a28fa10f1b2b1b85dd7836cb27ee
jamest11 <jameswtory@gmail.com>
2023-02-17 14:01:17 -0500

    Update routes

--- routes/instructor.js
+++ routes/instructor.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const Class = require('../models/class');
+const Quiz = require('../models/quiz');
+const ScheduledQuiz = require('../models/scheduled-quiz');
 
 const routes = express.Router()
 
@@ -30,13 +32,66 @@ routes.post('/update-class', async (req, res) => {
 
     await Class.updateOne({ '_id': classId }, { '$addToSet': { 'students': studentId } },  { runValidators: true })
 
-    const quizClass = await Class.findOne(mongoose.Types.ObjectId(req.body.class));
+    const quizClass = await Class.findOne(classId);
 
     return res.status(200).json(quizClass);
   } catch (err) {
     console.log(err);
-    return res.status(500).send();
+    return res.status(500).json(err);
   }
 });
- 
+
+routes.post('/create-quiz', async(req, res) => {
+  try {
+    const quiz = new Quiz(req.body);
+
+    await quiz.save();
+
+    return res.status(200).send();
+  } catch(err) {
+    console.log(err)
+    return res.status(500).json(err);
+  }
+});
+
+routes.post('/schedule-quiz', async (req, res) => {
+  try {
+    const scheduledQuiz = new ScheduledQuiz(req.body);
+
+    await scheduledQuiz.save().then((sq) => {
+      //sq.populate('class')
+    });
+
+    return res.status(200).json(scheduledQuiz);
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+routes.get('/scheduled-quizzes', async (req, res) => {
+  try {
+    const scheduledQuizzes = await ScheduledQuiz
+      .find()
+      .populate('class', ['title', 'description'])
+      .populate('quiz', 'title');
+
+    return res.status(200).json(scheduledQuizzes);
+  }  catch(err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+routes.get('/quizzes', async (req, res) => {
+  try {
+    const quizzes = await Quiz.find();
+
+    return res.status(200).json(quizzes);
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = routes
