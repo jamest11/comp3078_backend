36a2fa493c36725703840c6091362eec3a84b8fc
jamest11 <jameswtory@gmail.com>
2023-02-21 20:06:35 -0500

    Update instructor routes

--- routes/instructor.js
+++ routes/instructor.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 const User = require('../models/user');
 const Class = require('../models/class');
@@ -21,12 +22,8 @@ routes.post('/create-class', async (req, res) => {
   try {
     const instructor = decodeToken(req.headers);
 
-    const quizClass = new Class({ 
-      title: req.body.title, 
-      description: req.body.description, 
-      instructor: instructor.id
-    });
-    
+    const quizClass = new Class({ ...req.body, instructor: instructor.id });
+
     await quizClass.save();
 
     return res.status(201).json(quizClass);
@@ -126,5 +123,92 @@ routes.get('/classes', async (req, res) => {
     return res.status(500).json(err);
   }
 });
+
+routes.get('/quiz-grades', async (req, res) => {
+  try {
+    const instructor = decodeToken(req.headers);
+
+    const scheduledQuizzes = await ScheduledQuiz
+      .find()
+      .populate('class', ['title', 'description'], { instructor: { $eq: instructor.id }})
+      .populate('quiz', 'title')
+      .then((sqs) => sqs.filter((sq) => sq.class !== null));
+
+    const grades = [];
+
+    for(let quiz of scheduledQuizzes) {
+      if(quiz.grades.length > 0) {
+        let average = 0;
+        for(let grade of quiz.grades) {
+          average += grade.grade;
+        }
+
+        if(average > 0){
+          average /= quiz.grades.length;
+        }
+
+        grades.push({
+          id: quiz._id,
+          class: quiz.class.title,
+          class_id: quiz.class._id,
+          quiz: quiz.quiz.title,
+          dueDate: quiz.date,
+          average: average.toFixed(1),
+          completed: quiz.grades.length
+        });
+      }
+    }
+
+    return res.status(200).json(grades);
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+routes.get('/class-grades', async (req, res) => {
+  try {
+    const instructor = decodeToken(req.headers);
+
+    const classes = await Class.find({ instructor: instructor.id }, '_id, title')
+
+    const quizGrades = await axios.get('http://localhost:4000/instructor/quiz-grades', { 
+      headers: { authorization: req.headers.authorization },
+    });
+
+    const classGrades = []
+
+    for(let quizClass of classes) {
+      const classGrade = {
+        id: quizClass.id,
+        class: quizClass.title
+      }
+
+      let count = 0;
+      let total = 0;
+
+      for(let grade of quizGrades.data) {
+        if(grade.class_id == quizClass._id) {
+          count += 1;
+          total += parseFloat(grade.average);
+        }
+      }
+      let average = 0;
+
+      if(count > 0 && total > 0) {
+        average = total / count;
+      } 
+
+      classGrade.count = count;
+      classGrade.average = average;
+      classGrades.push(classGrade);
+    }
+    
+    res.status(200).json(classGrades);
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+})
 
 module.exports = routes;
