ef0dfb8ab50ae450ba33d7d203ca647effc1beba
jamest11 <jameswtory@gmail.com>
2023-04-01 13:01:18 -0400

    Implement email verification

--- models/user.js
+++ models/user.js
@@ -12,7 +12,8 @@ const userSchema = new mongoose.Schema({
     },
     password: {
       type: String,
-      required: true
+      required: true,
+      minLength: 6
     },
     firstName: {
       type: String,
--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,92 @@
-const express = require("express");
+const express = require('express');
+const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 const routes = express.Router()
 
+
+routes.get('/verify/:token', async (req, res) => {
+    try {
+        const token = req.params.token;
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+
+        const user = new User(decoded);
+        await user.validate();
+
+        bcrypt.hash(user.password, parseInt(process.env.SALT_ROUNDS), async (err, hash) => {
+            if(err) {
+                console.log(err);
+                return res.status(500).send('Password hashing failed');
+            }
+    
+            user.password = hash;
+    
+            try {
+                await user.save();
+    
+                return res.status(200).send('Account verified');
+            } catch(saveErr) {
+                console.log(saveErr.message);
+                return res.status(400).send('Invalid verification token');
+            }
+        });
+        
+    } catch(err) {
+        console.log(err.message);
+        return res.status(400).send('Invalid verification token');
+    }
+})
+
 routes.post('/register', async (req, res) => {
-    const user = new User(req.body);
+    let user;
 
-    bcrypt.hash(user.password, parseInt(process.env.SALT_ROUNDS), async (err, hash) => {
-        if(err) {
-            console.log(err);
-            return res.status(400).json({ errorCode: 103, message: 'Invalid password'});
+    try {
+        user = await User.findOne({ email: req.body.email });
+        if(user) {
+            return res.status(400).send('Email in use');
         }
 
-        user.password = hash;
+        user = new User(req.body);
+        await user.validate();
 
-        try {
-            await user.save();
+    } catch(err) {
+        return res.status(400).send('Validation failed');
+    }
 
-            return res.status(201).json(user);
-        } catch(saveErr) {
-            return res.status(500).send(saveErr);
-        }
-    });
+    try {
+        const userJSON = user.toJSON();
+        delete userJSON._id;
+
+        const token = jwt.sign(userJSON, process.env.TOKEN_KEY, { expiresIn: '24h', })
+        const url = process.env.ENVIRONMENT === 'dev' ? `http://localhost:3000/verify/${token}` : `https://quiztionnaire.tech/verify/${token}`
+
+        const transporter = nodemailer.createTransport({
+            service: 'Zoho',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: 'jameswtory@gmail.com',
+            subject: 'Quiztionnaire - Verify Account',
+            text: `Use this link to verify your Quiztionnaire account: ${url}`,
+            html: `<p>Click <a href="${url}" target="_blank">here</a> to verify your Quiztionnaire account.</p>`
+        });
+        
+        return res.status(200).send('Email sent');
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send('Error sending email');
+    }
 });
     
 routes.post('/login', async (req, res) => {
+    console.log(process.env.NODE_ENV)
     try {
         const { email, password } = req.body;
         let user = null;
@@ -48,7 +108,10 @@ routes.post('/login', async (req, res) => {
                     user: {
                         id: user._id,
                         userType: user.userType,
-                        email: user.email
+                        email: user.email,
+                        createdAt: user.createdAt,
+                        firstName: user.firstName,
+                        lastName: user.lastName
                     }
                 };
 
